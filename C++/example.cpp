2e8f2f547208a8a39b8811e19140dcd5458e6f60
Martin Andreas Osnes <Martin.a.osnes@gmail.com>
2018-09-27 10:34:05 +0200

    Added c++ files

---
+++ C++/example.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std; // simplifies commands
+
+int main () {
+
+    int primes[5] = {2, 3, 5, 7, 11};
+
+    cout << primes[4] << endl;
+    system("pause>nul"); // pause window
+    return 0;
+}
---
+++ C++/hello_world.cpp
@@ -0,0 +1,13 @@
+// print ("Hello, world!")
+
+#include <iostream>
+#include <string> // not included
+
+using namespace std; // allows removal of "std::"
+
+int main() {
+    cout << "Hello world" << endl;
+    system("pause>nul"); //pause the program, preventing terminal from closing. 
+// alternately std::cout, std::endl
+    return 0; // denotes successfull run, 'exit code 0'
+}
